57c10399aa9c9b43d9f98c142740609d6ece2aa4
k4keye <vps32@naver.com>
2020-08-31 21:11:39 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Csharp-StrCutDLL
+:speech_balloon: C# Find only the desired location in the string , Bring money in a typical way.
+
+
+## StrCut
+### Description
+Find only the desired location in the string  
+Bring money in a typical way.
+Parameter : True = integer find  , False : All Data
+
+### instruction
+-----------------------------------
+![carbon (1)](https://user-images.githubusercontent.com/52993842/91717858-4532c680-ebcd-11ea-9e67-1e2a8f43f7d6.png)
+
+
+
+### Result
+-----------------------------------
+![carbon (3)](https://user-images.githubusercontent.com/52993842/91713621-13b5fd00-ebc5-11ea-8edd-660526a700f0.png)
+
---
+++ StrCutDLL/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace StrCutDLL
+{
+    public class StrCut
+    {
+        /// <summary>
+        /// Find only the desired location in the string
+        /// If you choose the wrong location return "ERR"
+        /// </summary>
+        /// <param name="Original">Original String</param>
+        /// <param name="strStart">Start of source string to find or Enter NULL to 0 index</param>
+        /// <param name="strEnd">End of source string to find, or Enter NULL to End index</param>
+        /// <param name="flag">True : integer find  , False : All </param>
+        public static string StrChange(string Original, string strStart, string strEnd, bool flag)
+        {
+            int indexStart = 0;
+            StringBuilder resultStr = new StringBuilder();
+            if (strStart != null)
+            {
+                indexStart = Original.IndexOf(strStart);
+                if (indexStart == -1) throw new Exception("strStart Check it out"); //no strStart
+                indexStart += strStart.Length;
+            }
+            else indexStart = 0;
+
+
+            if (strEnd != null)
+            {
+                int indexEnd = Original.IndexOf(strEnd, indexStart) - indexStart;
+                if (indexEnd < 0) throw new Exception("strEnd Check it out"); //no strEnd
+                Original = Original.Substring(indexStart, indexEnd);
+            }
+            else Original = Original.Substring(indexStart);
+
+            if (flag == false) return Original;
+
+            for (int i = 0; i < Original.Length; i++)
+            {
+                int htmlChar = Convert.ToInt32(Original[i]);
+                if (htmlChar >= 48 && htmlChar <= 57) resultStr.Append(Original[i]);
+                else if (resultStr.Length != 0 && (htmlChar < 48 || htmlChar > 57) && htmlChar != 44) break;
+            }
+            if (resultStr.Length == 0) throw new Exception("All Check it out"); //no Value
+            return resultStr.ToString();
+        }
+    }
+}
