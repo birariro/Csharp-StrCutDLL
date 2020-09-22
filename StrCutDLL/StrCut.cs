b3235533ff64b7bdaa62a1e485cc9bbd32430428
k4keye <vps32@naver.com>
2020-09-22 18:10:17 +0900

    StrChangeArray

--- StrCutDLL/StrCut.cs
+++ StrCutDLL/StrCut.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace StrCutDLL
@@ -49,6 +50,46 @@ namespace StrCutDLL
         {
             string[] result = Original.Split(new string[] { SplitStr }, StringSplitOptions.None);
             return result;
+        }
+
+        /// <summary>
+        /// Find only numbers in a string
+        /// Number of Multiple
+        /// </summary>
+        /// <param name="Original">Original String</param>
+        /// <param name="strStart">Start of source string to find or Enter NULL to 0 index</param>
+        /// <param name="strEnd">End of source string to find, or Enter NULL to End index</param>
+        public static string[] StrChangeArray(string Original, string strStart, string strEnd)
+        {
+            try
+            {
+                Original = StrChange(Original, strStart, strEnd, false);
+                List<string> resultList = new List<string>();
+                string strTmp = "";
+                for (int i = 0; i < Original.Length; i++)
+                {
+                    int htmlChar = Convert.ToInt32(Original[i]);
+                    if (htmlChar >= 48 && htmlChar <= 57) strTmp += Original[i];
+                    else if (strTmp.Length != 0 && (htmlChar < 48 || htmlChar > 57) && htmlChar != 44)
+                    {
+                        resultList.Add(strTmp);
+                        strTmp = "";
+                    }
+                }
+                string[] result = new string[resultList.Count];
+                for (int i = 0; i < resultList.Count; i++)
+                {
+                    result[i] = resultList[i];
+                }
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                DebugLog.e(e);
+                return new string[] { "ERR" };
+            }
+
         }
     }
 }
